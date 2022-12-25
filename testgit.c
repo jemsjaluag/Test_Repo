c574ded10646a3bf1abc608c81adfb8d7fc1b5dd
James Jaluag <james_jaluag@yahoo.com>
2022-12-25 09:02:21 -0500

    correction

--- testgit.c
+++ testgit.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv) {
 	
 	if(argc > 1){
-		printf("You entered: %d\n", atoi(argv[1][0]));
+		printf("You entered: %d\n", atoi(argv[1]));
 	}
 	else printf("Need arguments, dude.\n");
 }
